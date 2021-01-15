637ce27fc6f50f6cd2fbcfbeb6b0a946802baa6b
Tito Schmidt <tito.the.titan@gmail.com>
2021-01-15 19:26:51 -0300

    projeto api-rest reiniciado em Spring Boot
    *Java 11
    *Spring 2.4.0
    
    *JPA
    *H2
    *Devtools
    *Spring Web

---
+++ api-candidatos/src/main/java/com/titoschmidt/apicandidatos/ApiCandidatosApplication.java
@@ -0,0 +1,13 @@
+package com.titoschmidt.apicandidatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCandidatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCandidatosApplication.class, args);
+	}
+
+}
