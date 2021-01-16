afa3f5ee6679f30dc27ad7370c63c39d55db2f45
Tito Schmidt <tito.the.titan@gmail.com>
2021-01-16 10:56:30 -0300

    Criado 'CandidatoRepository'

---
+++ api-candidatos/src/main/java/com/titoschmidt/apicandidatos/repositories/CandidatoRepository.java
@@ -0,0 +1,11 @@
+package com.titoschmidt.apicandidatos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.titoschmidt.apicandidatos.entities.Candidato;
+
+@Repository
+public interface CandidatoRepository extends JpaRepository<Candidato, Long> {
+
+}
